c8abced3962c4a102c5306fcf2d3151f9b34f015
navdeepisno1 <49776626+navdeepisno1@users.noreply.github.com>
2021-01-09 22:54:29 +0530

    Initial commit

---
+++ app/src/main/java/com/suvidha/pokedex/MainActivity.java
@@ -0,0 +1,30 @@
+package com.suvidha.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                change();
+            }
+        },1500);
+    }
+
+    private void change()
+    {
+        startActivity(new Intent(context,dashboard.class));
+        finish();
+    }
+}
